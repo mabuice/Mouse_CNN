227e316ec4ad71af07b185418f70f3dfdbf08929
zjjxsjh <zjjxsjh@gmail.com>
2019-07-24 14:24:59 -0700

    move test_example to gen_anatomy

--- cmouse/gen_anatomy.py
+++ cmouse/gen_anatomy.py
@@ -1,13 +1,27 @@
-# -*- coding: utf-8 -*-
-from cmouse.anatomy import *
-from cmouse.augdata import *
+import sys
+sys.path.append('../')
+from mouse_cnn.data import *
+from anatomy import *
 
-def test_example():
-    # settings
-    input_depths = ['4']
-    output_depths = ['2/3', '5']
-    lamimar_connections = [('4', '2/3'), ('2/3', '5')]
+class AugData(Data):
+    def get_kernel_width(self, source_area, source_layer, target_area, target_layer):
+        """
+        Kernel width associated with an inter-area connection, estimated from voxel model.
 
+        :param source_area: name of source visual area (e.g. 'VISp')
+        :param source_layer: name of source layer (e.g. '2/3)
+        :param target_area: name of target visual area (e.g. 'VISrl')
+        :param target_layer: name of target layer (e.g. '4')
+        :return: kernel width (micrometers)
+        """
+        # TODO: this is here due to a circular dependence between data and voxel (whoops)
+        # TODO: consider removing this method; it just wraps target.get_kernel_width
+
+        return 1000
+
+def gen_anatomy(input_depths = ['4'],
+                output_depths = ['2/3', '5'],
+                lamimar_connections = [('4', '2/3'), ('2/3', '5')]):
     anet = AnatomicalNet()
     data = AugData()
     areas = data.get_areas()
---
+++ cmouse/tests/test_gen_anatomy.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from cmouse.anatomy import *
+from cmouse.gen_anatomy import *
+
+def test_gen_anatomy():
+    return gen_anatomy()
